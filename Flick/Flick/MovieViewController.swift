6857cf87d5294b28d0a61e971f31da3aa68e0eef
SzuKaiYang <bbkai2857@gmail.com>
2017-02-06 03:48:41 -0500

    Added some additional function

--- Flick/Flick/MovieViewController.swift
+++ Flick/Flick/MovieViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 import AFNetworking
+import MBProgressHUD
 
-class MovieViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class MovieViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -17,7 +18,12 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    
+        //Adding the progress HUD when load
+        let loadingWarning = MBProgressHUD.showAdded(to: self.view, animated: true)
+        loadingWarning.mode = MBProgressHUDMode.indeterminate
+        loadingWarning.label.text = "Loading"
+        
         //Tell program that protocol functions is here
         tableView.dataSource = self
         tableView.delegate = self
@@ -28,15 +34,37 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         //Call the refreshControlAction function when ControlEvents sense value change and start the spinning
         refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
         
+        //Set up the URL request
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        
+        //Setup the session
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        //Make the request
         let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            guard error == nil else {
+                
+                let alert = UIAlertController(title: " Error", message: "No internet connection", preferredStyle: .alert )
+                self.present(alert, animated: true, completion: nil)
+                let OKAction = UIAlertAction(title: "OK", style: .default){(action:UIAlertAction) in
+                    print("OK");
+                    return
+                }
+                alert.addAction(OKAction)
+            return
+            }
+
+            
             if let data = data {
                 //dataDictionary = the file of json. Its dictionary
                 if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
-                    print(dataDictionary)
+                    //print(dataDictionary)
+                    
+                    //Hide progress HUD after load
+                    MBProgressHUD.hide(for: self.view, animated: true)
+                    
                     //movies = all of the data after results
                     self.movies = dataDictionary["results"] as? [NSDictionary]
                     self.tableView.reloadData()
@@ -45,6 +73,7 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
             //Add spinning icon to the table view
             self.tableView.insertSubview(refreshControl, at: 0)
         }
+        
         task.resume()
         
     }
@@ -54,15 +83,24 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
      */
    func refreshControlAction(_ refresh: UIRefreshControl) {
     
+    //Adding the progress HUD when load
+    let loadingWarning = MBProgressHUD.showAdded(to: self.view, animated: true)
+    loadingWarning.mode = MBProgressHUDMode.indeterminate
+    loadingWarning.label.text = "Loading"
+    UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    
     let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
     let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
     let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
     let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-    let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
         if let data = data {
             //dataDictionary = the file of json. Its dictionary
             if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
-                print(dataDictionary)
+        
+                //Hide progress HUD after load
+                MBProgressHUD.hide(for: self.view, animated: true)
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
                 //movies = all of the data after results
                 self.movies = dataDictionary["results"] as? [NSDictionary]
                 self.tableView.reloadData()
@@ -99,7 +137,6 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         
     }
     
-    
     //Communicate and set the content of each cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         
@@ -123,5 +160,25 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         
         return cell
     }
-
+    
+    func isInternetAvailable() -> Bool
+    {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
+    }
 }
